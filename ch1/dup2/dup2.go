c9fbca7626054bd32870b39a204c18191c37b4f2
David Garcia-Campos <dgarciacampos@gmail.com>
2020-04-17 08:16:14 -0400

    Excercise 1.4
    Modify dup2 to print the names of all files in which each duplicated
    line occurs.

--- ch1/dup2/dup2.go
+++ ch1/dup2/dup2.go
@@ -7,31 +7,42 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, "Stdin")
 	} else {
+
 		for _, arg := range files {
 			fd, err := os.Open(arg)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 			}
-			countLines(fd, counts)
+			countLines(fd, counts, arg)
 			fd.Close()
-		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("Evaluating file %s\n", arg)
+			printLines(counts)
+			fmt.Printf("Done\n")
+			counts = make(map[string]map[string]int)
 		}
 	}
 }
-
-func countLines(fd *os.File, counts map[string]int) {
+func printLines(counts map[string]map[string]int) {
+	for file, lineNumberMap := range counts {
+		for line, n := range lineNumberMap {
+			if n > 1 {
+				fmt.Printf("%s\t%d\t%s\n", file, n, line)
+			}
+		}
+	}
+}
+func countLines(fd *os.File, counts map[string]map[string]int, file string) {
 	input := bufio.NewScanner(fd)
 	for input.Scan() {
-		counts[input.Text()]++ //save results in counts
+		if counts[file] == nil {
+			counts[file] = map[string]int{}
+		}
+		counts[file][input.Text()]++ //save results in counts
 	}
 
 }
