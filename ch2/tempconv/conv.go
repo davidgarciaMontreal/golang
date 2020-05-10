82e963f3c8dd6f8cd7421d753499501490ceb137
David Garcia-Campos <dgarciacampos@gmail.com>
2020-05-10 13:03:51 -0400

    Excercise 2.1
    Add types, constants, and functions to...

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -1,4 +1,21 @@
 package tempconv
 
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c + 273)
+}
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+func FToK(f Fahrenheit) Kelvin {
+	return Kelvin((f-32)*5/9 + 273)
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius((k - 273))
+}
+func KToF(k Kelvin) Fahrenheit {
+	return Fahrenheit((k-273)*9/5 + 273)
+}
